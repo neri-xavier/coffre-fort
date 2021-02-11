8dc98930c92e77ba9bcb78fa5968daa830481a08
neri-xavier <78912393+neri-xavier@users.noreply.github.com>
2021-02-11 14:58:00 +0100

    Delete coffre/coffre directory

--- coffre/coffre/lib/navbar.php
+++
@@ -1,7 +0,0 @@
-<nav>
-                <ul>
-                    <ol><a href="./informations.php">Mes informations</a></ol>
-                    <ol><a href="./fichiers.php">Mes fichiers</a></ol>
-                    <ol id="deco"><a href="./connexion.php">Deconnexion</a></ol>
-                </ul>
-</nav>
