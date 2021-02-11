8792214c05bdc69ed862da72c159a626899012bf
neri-xavier <78912393+neri-xavier@users.noreply.github.com>
2021-02-11 19:04:44 +0100

    Add files via upload

--- coffre/connexion.php
+++ coffre/connexion.php
@@ -1,24 +1,31 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link href="style/connexion.css" rel="stylesheet">
         <title>Page d'inscription</title>
     </head>
     <body>
-    <form action="php/connexionBDD.php" method="post" class="form-inscription">
-        <div class="form-inscription">
-            <label for="email">Saisir votre email: </label>
-            <input type="email" name="email" id="email" pattern="[^\s]+" required>
+        <div class="container">
+            <div class="connexion">
+                <form action="php/connexionBDD.php" method="post" class="form-inscription">
+                    <div class="champs-connexion">
+                        <label for="email">Saisir votre email: </label>
+                        <input type="email" name="email" id="email" pattern="[^\s]+" required>
+                    </div>
+                </br>
+                    <div class="champs-connexion">
+                        <label for="pass">Password (8 characters minimum):</label>
+                        <input type="password" id="password" name="password" minlength="8" pattern="[^\s]+" required>
+                    </div>
+                </br>
+                    <button type="submit">Connexion</button>
+                </form>
+            </div>
+        </br>
+            <div class="liens">
+                <p><a href="inscription.php">Inscription</a></p>
+                <p><a href="verif.php">Mot de passe oublié</a></p>
+            </div>
         </div>
-    </br>
-        <div class="form-password">
-            <label for="pass">Password (8 characters minimum):</label>
-            <input type="password" id="password" name="password" minlength="8" pattern="[^\s]+" required>
-        </div>
-    </br>
-        <button type="submit">Connexion</button>
-    </form>
-</br>
-    <p><a href="inscription.php">Inscription</a></p>
-    <p><a href="verif.php">Mot de passe oublié</a></p>
     </body>
 </html>
