42e4a386b5ab9dbbb16dc3d2f8fbef998b45a71c
neri-xavier <78912393+neri-xavier@users.noreply.github.com>
2021-02-11 14:02:55 +0100

    Add files via upload

---
+++ coffre/informations.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="style/test.css" rel="stylesheet">
+        <title>Connecté</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <ol><a href="./informations.php">Mes informations</a></ol>
+                    <ol><a href="./fichiers.php">Mes fichiers</a></ol>
+                    <ol id="deco"><a href="./connexion.php">Deconnexion</a></ol>
+                </ul>
+            </nav>
+        </header>
+        <p>Voici vos informations</p>
+    </body>
+</html>
