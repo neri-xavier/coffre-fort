2d9c67ae84314c230e7285ed12a5c15b9a3027e2
neri-xavier <78912393+neri-xavier@users.noreply.github.com>
2021-02-11 14:58:29 +0100

    Add files via upload

--- coffre/connected.php
+++ coffre/connected.php
@@ -1,7 +1,4 @@
 <!DOCTYPE html>
-<?php
-
-?>
 <html>
     <head>
         <link href="style/test.css" rel="stylesheet">
@@ -9,13 +6,14 @@
     </head>
     <body>
         <header>
-            <nav>
+            <!--<nav>
                 <ul>
                     <ol><a href="./informations.php">Mes informations</a></ol>
                     <ol><a href="./fichiers.php">Mes fichiers</a></ol>
                     <ol id="deco"><a href="./connexion.php">Deconnexion</a></ol>
                 </ul>
-            </nav>
+            </nav>-->
+            <?php include "lib/navbar.php";?>
         </header>
         <p>Vous êtes connecté</p>
     </body>
--- coffre/fichiers.php
+++ coffre/fichiers.php
@@ -6,13 +6,7 @@
     </head>
     <body>
         <header>
-            <nav>
-                <ul>
-                    <ol><a href="./informations.php">Mes informations</a></ol>
-                    <ol><a href="./fichiers.php">Mes fichiers</a></ol>
-                    <ol id="deco"><a href="./connexion.php">Deconnexion</a></ol>
-                </ul>
-            </nav>
+            <?php include "lib/navbar.php";?>
         </header>
         <p>Voici vos fichiers</p>
     </body>
