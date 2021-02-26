013e2f260188d283846de8f9cf8f1950fe71deb3
neri-xavier <78912393+neri-xavier@users.noreply.github.com>
2021-02-26 11:15:34 +0100

    Add files via upload

--- coffre-fort/inscription.php
+++ coffre-fort/inscription.php
@@ -11,37 +11,31 @@ include 'php/verifConnexion.php';
         <div class="container">
             <form action="php/inscriptionBDD.php" method="post" class="form-inscription">
                 <div class="inscription">
-                    <label for="name">Saisir votre nom : </label>
-                    <input type="text" name="nom" id="nom" pattern="[^\s]+" required>
+                    <input type="text" name="nom" id="nom" placeholder="Saisir votre nom" pattern="[^\s]+" required>
                 </div>
             </br>
             <div class="inscription">
-                    <label for="email">Saisir votre prénom : </label>
-                    <input type="text" name="prenom" id="prenom" pattern="[^\s]+" required>
+                    <input type="text" name="prenom" id="prenom" placeholder="Saisir votre prénom" pattern="[^\s]+" required>
                 </div>
             </br>
                 <div class="inscription">
-                    <label for="email">Saisir votre email : </label>
-                    <input type="email" name="email" id="email" pattern="[^\s]+" required>
+                    <input type="email" name="email" id="email" placeholder="Saisir votre email"pattern="[^\s]+" required>
                 </div>
             </br>
                 <div class="inscription">
-                    <label for="phone">Enter your phone number :</label>
-                    <input type="tel" id="phone" name="phone" pattern="[0]{1}[0-9]{9}" required>
+                    <input type="tel" id="phone" name="phone" placeholder="Saisir votre numéro" pattern="[0]{1}[0-9]{9}" required>
             </div>
             </br>
                 <div class="inscription">
-                    <label for="pass">Password (8 characters minimum) :</label>
-                    <input type="password" id="password" name="password" minlength="8" pattern="[^\s]+" required>
+                    <input type="password" id="password" name="password" minlength="8" placeholder="Saisir un mot de passe" pattern="[^\s]+" required>
                 </div>
             </br>
             <div class="inscription">
-                    <label for="pass">Password (8 characters minimum) :</label>
-                    <input type="password" id="password-confirm" name="password-confirm" minlength="8" pattern="[^\s]+" required>
+                    <input type="password" id="password-confirm" name="password-confirm" minlength="8" placeholder="confirmer le mot de passe"pattern="[^\s]+" required>
                 </div>
             </br>
             <input type="hidden" id="recaptchaResponse" name="recaptcha-response">
-                <button type="submit">Inscription</button>
+                <button type="submit"><div id="text-inscription">Inscription</div></button>
             </form>
        
     
